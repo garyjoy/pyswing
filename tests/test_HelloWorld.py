908e8029ba1482ed4f09032e228b41b008e6556f
garyjoy <gary.joy@gmail.com>
2015-09-01 23:17:58 +1000

    Initial Commit.

---
+++ tests/test_HelloWorld.py
@@ -0,0 +1,16 @@
+import unittest
+from pyswing.HelloWorld import HelloWorld
+
+
+class TestHelloWorld(unittest.TestCase):
+
+  def test_HelloWorld(self):
+
+      HelloWorld()
+      pass
+
+
+if __name__ == '__main__':
+    unittest.main()
+
+
